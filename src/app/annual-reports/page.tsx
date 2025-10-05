43c15db7f7ebb1b108d8360d43d8d033285d386e
mrithunjay tanish shanmuganand <139517421+mrithunjay26@users.noreply.github.com>
2025-10-05 13:20:19 -0700

    Update page.tsx

--- src/app/annual-reports/page.tsx
+++ src/app/annual-reports/page.tsx
@@ -16,6 +16,7 @@ import Link from "next/link"
 import { Document, Page, pdfjs } from "react-pdf"
 import "react-pdf/dist/Page/AnnotationLayer.css"
 import "react-pdf/dist/Page/TextLayer.css"
+export const dynamic = "force-dynamic"
 
 // Configure PDF.js worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
